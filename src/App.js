932efba5347b6a4651399e2d9a2cd67fb820da21
Kumar Utkarsh Das <116294098+Utkarshdas24@users.noreply.github.com>
2024-09-09 11:48:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
 
             <UTCDatetime />
             <Link
-              href="https://github.com/Amin-Awinti"
+              href="https://github.com/Utkarshdas24"
               target="_blank"
               underline="none"
               sx={{ display: 'flex' }}
